63756a237f78ec3eda5737da43b01d66bddb02aa
dev <pawan.patidar@papasiddhi.com>
2020-10-29 23:02:59 +0530

    migration changes

---
+++ database/migrations/2020_10_29_171138_create_teams_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('designation');
+            $table->string('image');
+            $table->string('address');
+            $table->string('bio');
+            $table->string('personal_info');
+            $table->string('facebook');
+            $table->string('twitter');
+            $table->string('instagram');
+            $table->string('skype');
+            $table->string('cover_image');
+            $table->string('profile_image');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
