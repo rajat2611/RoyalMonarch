da7cbfa19bd980ba77aca7243e62da3ed138fff6
dev <pawan.patidar@papasiddhi.com>
2020-11-06 14:52:42 +0530

    migration

--- database/migrations/2016_02_15_204651_create_categories_table.php
+++ database/migrations/2016_02_15_204651_create_categories_table.php
@@ -3,22 +3,21 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateCategoriesTable extends Migration
-{
+class CreateCategoriesTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         // Create table for storing categories
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('parent_id')->unsigned()->nullable()->default(null);
-            $table->foreign('parent_id')->references('id')->on('categories')->onUpdate('cascade')->onDelete('set null');
+            $table->foreign('parent_id')->references('id')->on('categories')->onUpdate('cascade')->onDelete('set null')->default(null);
             $table->integer('order')->default(1);
-            $table->string('name');
+            $table->string('title');
             $table->string('slug')->unique();
             $table->boolean('status');
             $table->timestamps();
@@ -30,8 +29,8 @@ class CreateCategoriesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::drop('categories');
     }
+
 }
